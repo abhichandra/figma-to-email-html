07ab957ddd7e790f38bbc5badc7cf320863e90cc
abhichandra <sunilschandra@gmail.com>
2025-06-12 17:32:29 +0530

    Update emailTemplate.js

--- templates/emailTemplate.js
+++ templates/emailTemplate.js
@@ -1,23 +1,30 @@
 function generateEmailTable(components) {
   let rows = components.map(comp => {
     if (comp.type === 'text') {
-      return `<tr><td style="font-size:14px; color:#333333;">${comp.content}</td></tr>`;
+      const style = comp.style || {};
+      return \`<tr><td style="
+        font-size: \${style.fontSize}px;
+        font-weight: \${style.fontWeight};
+        text-align: \${style.textAlign};
+        color: \${style.color};
+        font-family: Arial, sans-serif;
+      ">\${comp.content}</td></tr>\`;
     }
     if (comp.type === 'image') {
-      return `<tr><td><img src="${comp.url}" width="100%" style="display:block;" /></td></tr>`;
+      return \`<tr><td><img src="\${comp.url}" width="100%" style="display:block;" /></td></tr>\`;
     }
     return '';
   }).join('\n');
 
-  return `
+  return \`
   <!DOCTYPE html>
   <html>
   <body>
     <table border="0" cellspacing="0" cellpadding="0" style="border-collapse:collapse; width:100%;">
-      ${rows}
+      \${rows}
     </table>
   </body>
-  </html>`;
+  </html>\`;
 }
 
 module.exports = { generateEmailTable };
